6362b27940d01a5ae30cdb5cab66c4bc77dfa954
Zephyr <lococeq.1985@gmail.com>
2017-08-24 18:12:30 -0500

    Improved implementation of the Stream2Multiple writable

--- index.js
+++ index.js
@@ -20,6 +20,31 @@ class Stream2Multiple extends stream.Writable {
 
 		// store destinations for latter use
 		this.destinations = destinations;
+
+		// setup error handling
+		destinations.forEach((destination) => {
+			destination.on('error', (err) => this.emit('error', err));
+		});
+
+		// setup writer end transmission
+		this.on('finish', () => {
+			destinations.forEach((destination) => destination.end());
+		});
+	}
+
+	_promiseToWrite(destination, chunk, encoding){
+		return new Promise((resolve, reject) => {
+			destination.write(chunk, encoding, (err) => {
+				if(err){ return reject(err); }
+				else { resolve(); }
+			});
+		});
+	}
+
+	_write(chunk, encoding, callback){
+		Promise.all(this.destinations.map(
+			(destination) => this._promiseToWrite(destination, chunk, encoding)))
+			.then(() => callback(), (err) => callback(err));
 	}
 }
 
